281c762b6c7c72d866ea32743533b56abf93d0ce
Tom <49661849+tomltr@users.noreply.github.com>
2019-09-09 08:33:55 -0700

    Refactor Expense

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -2,23 +2,25 @@ import React from 'react';
 import RemoveExpense from './RemoveExpense';
 import EditExpense from './EditExpense';
 
+
+// A single expense row contains its info along with EditExpense and RemoveExpense Component
 const Expense = (props) =>
 {
    return (
-       <tr>
-        <td>{props.expenseName}</td>
-        <td>{props.expenseType}</td>
-        <td>${props.expenseAmount}</td>
-        <td>{props.dateCreated}</td>
+        <tr>
+            <td>{props.expenseName}</td>
+            <td>{props.expenseType}</td>
+            <td>${props.expenseAmount}</td>
+            <td>{props.dateCreated}</td>
 
-        <EditExpense expenses={props.expenses}
-                    toggleEdit={props.toggleEdit}
-                    expenseId={props.expenseId}/>
+        <EditExpense    expenses={props.expenses}
+                        toggleEdit={props.toggleEdit}
+                        expenseId={props.expenseId}/>
 
-        <RemoveExpense expenseId={props.expenseId}
-                       expenseAmount={props.expenseAmount}
-                       removeExpense={props.removeExpense}/>
-       </tr>
+        <RemoveExpense  expenseId={props.expenseId}
+                        expenseAmount={props.expenseAmount}
+                        removeExpense={props.removeExpense}/>
+        </tr>
    ); 
 }
 
